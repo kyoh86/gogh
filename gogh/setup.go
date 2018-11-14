8f25cbdcf592e91da358f3601864d0199319f753
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2018-11-14 21:51:02 +0900

    support completions for zsh / bash
    fix: #4

--- gogh/setup.go
+++ gogh/setup.go
@@ -8,9 +8,22 @@ import (
 func Setup(cdFuncName, shell string) error {
 	_, shName := filepath.Split(shell)
 	switch shName {
-	case "zsh", "bash":
-		_, err := fmt.Printf(`function %s { cd $(gogh find $@) }`, cdFuncName)
-		return err
+	case "zsh":
+		if _, err := fmt.Printf(`function %s { cd $(gogh find $@) }%s`, cdFuncName, "\n"); err != nil {
+			return err
+		}
+		if _, err := fmt.Printf(`eval "$(gogh --completion-script-zsh)"%s`, "\n"); err != nil {
+			return err
+		}
+		return nil
+	case "bash":
+		if _, err := fmt.Printf(`function %s { cd $(gogh find $@) }%s`, cdFuncName, "\n"); err != nil {
+			return err
+		}
+		if _, err := fmt.Printf(`eval "$(gogh --completion-script-bash)"%s`, "\n"); err != nil {
+			return err
+		}
+		return nil
 	default:
 		return fmt.Errorf("unsupported shell %q", shell)
 	}
