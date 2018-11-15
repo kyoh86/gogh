b342644305a512810abfbf48e749416162370f27
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2018-11-15 15:25:17 +0900

    fix lints(cyclomatic complexity of gogh.Find)

--- gogh/find.go
+++ gogh/find.go
@@ -11,45 +11,57 @@ import (
 
 // Find a path of a local repository
 func Find(name string) error {
-	reposFound := []*repo.Local{}
-	if err := repo.Walk(func(repo *repo.Local) error {
-		if repo.Matches(name) {
-			reposFound = append(reposFound, repo)
-		}
-		return nil
-	}); err != nil {
+	repos, err := findRepos(name)
+	if err != nil {
 		return err
 	}
 
-	switch len(reposFound) {
+	switch len(repos) {
 	case 0:
-		spec, err := repo.NewSpec(name)
+		path, err := getRepoFullPath(name)
 		if err != nil {
 			return err
 		}
-		repo, err := repo.FromURL(spec.URL())
-		if err != nil {
-			return err
-		}
-
-		// if the directory exists
-		if info, err := os.Stat(repo.FullPath); err == nil && info.IsDir() {
-			fmt.Println(repo.FullPath)
-			return nil
-		}
-		return errors.New("no repository found")
+		fmt.Println(path)
 
 	case 1:
-		fmt.Println(reposFound[0].FullPath)
+		fmt.Println(repos[0].FullPath)
 
 	default:
 		var lines []string
 		lines = append(lines, "more than one repositories are found")
 		lines = append(lines, "try more precise name")
-		for _, repo := range reposFound {
+		for _, repo := range repos {
 			lines = append(lines, "- "+strings.Join(repo.PathParts, "/"))
 		}
 		return errors.New(strings.Join(lines, "\n"))
 	}
 	return nil
+}
+
+func findRepos(name string) ([]*repo.Local, error) {
+	var repos []*repo.Local
+	return repos, repo.Walk(func(repo *repo.Local) error {
+		if repo.Matches(name) {
+			repos = append(repos, repo)
+		}
+		return nil
+	})
+}
+
+func getRepoFullPath(name string) (string, error) {
+	spec, err := repo.NewSpec(name)
+	if err != nil {
+		return "", err
+	}
+	repo, err := repo.FromURL(spec.URL())
+	if err != nil {
+		return "", err
+	}
+
+	// if the directory exists
+	if info, err := os.Stat(repo.FullPath); err == nil && info.IsDir() {
+		return repo.FullPath, nil
+	}
+	return "", errors.New("no repository found")
 }
