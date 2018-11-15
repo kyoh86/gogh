b170399e60aaa7f586f118f2ac16ed81d1d1861c
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2018-11-15 14:51:04 +0900

    fix list test

--- gogh/list_test.go
+++ gogh/list_test.go
@@ -1,12 +1,20 @@
 package gogh
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestList(t *testing.T) {
+	tmp, err := ioutil.TempDir(os.TempDir(), "gogh-test")
+	require.NoError(t, err)
+	require.NoError(t, os.Setenv("GOPATH", tmp))
+	require.NoError(t, os.MkdirAll(filepath.Join(tmp, "src"), 0755))
+
 	t.Run("plain", func(t *testing.T) {
 		_, _, err := capture(func() {
 			require.NoError(t, List(false, false, false, false, ""))
