285e280e4a5bc45082e9f097d76b2f404c7213e3
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2018-11-19 10:32:43 +0900

    drop gomega

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -8,23 +8,18 @@ import (
 	"testing"
 
 	"github.com/kyoh86/gogh/internal/run"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGitBackend(t *testing.T) {
-	RegisterTestingT(t)
-
 	tempDir, err := ioutil.TempDir("", "gogh-test")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	localDir := filepath.Join(tempDir, "repo")
 
 	remoteURL, err := url.Parse("https://example.com/git/repo")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	commands := []*exec.Cmd{}
 	lastCommand := func() *exec.Cmd { return commands[len(commands)-1] }
@@ -34,27 +29,24 @@ func TestGitBackend(t *testing.T) {
 	}
 
 	err = Clone(remoteURL, localDir, false)
-
-	Expect(err).NotTo(HaveOccurred())
-	Expect(commands).To(HaveLen(1))
-	Expect(lastCommand().Args).To(Equal([]string{
+	require.NoError(t, err)
+	assert.Len(t, commands, 1)
+	assert.Equal(t, []string{
 		"git", "clone", remoteURL.String(), localDir,
-	}))
+	}, lastCommand().Args)
 
 	err = Clone(remoteURL, localDir, true)
-
-	Expect(err).NotTo(HaveOccurred())
-	Expect(commands).To(HaveLen(2))
-	Expect(lastCommand().Args).To(Equal([]string{
+	require.NoError(t, err)
+	assert.Len(t, commands, 2)
+	assert.Equal(t, []string{
 		"git", "clone", "--depth", "1", remoteURL.String(), localDir,
-	}))
+	}, lastCommand().Args)
 
 	err = Update(localDir)
-
-	Expect(err).NotTo(HaveOccurred())
-	Expect(commands).To(HaveLen(3))
-	Expect(lastCommand().Args).To(Equal([]string{
+	require.NoError(t, err)
+	assert.Len(t, commands, 3)
+	assert.Equal(t, []string{
 		"git", "pull", "--ff-only",
-	}))
-	Expect(lastCommand().Dir).To(Equal(localDir))
+	}, lastCommand().Args)
+	assert.Equal(t, localDir, lastCommand().Dir)
 }
