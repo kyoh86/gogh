818f880d4417df36af16c1fcc7f6974048173336
Kyoichiro Yamada <kyoh86@users.noreply.github.com>
2018-11-25 14:00:42 +0900

    enhance spec test

--- gogh/spec_test.go
+++ gogh/spec_test.go
@@ -7,7 +7,55 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestRepoShared(t *testing.T) {
+	t.Run("valid shareds", func(t *testing.T) {
+		var shared RepoShared
+		assert.NoError(t, shared.Set("false"))
+		assert.Equal(t, "false", shared.String())
+		assert.NoError(t, shared.Set("true"))
+		assert.Equal(t, "true", shared.String())
+		assert.NoError(t, shared.Set("umask"))
+		assert.Equal(t, "umask", shared.String())
+		assert.NoError(t, shared.Set("group"))
+		assert.Equal(t, "group", shared.String())
+		assert.NoError(t, shared.Set("all"))
+		assert.Equal(t, "all", shared.String())
+		assert.NoError(t, shared.Set("world"))
+		assert.Equal(t, "world", shared.String())
+		assert.NoError(t, shared.Set("everybody"))
+		assert.Equal(t, "everybody", shared.String())
+		assert.NoError(t, shared.Set("0777"))
+		assert.Equal(t, "0777", shared.String())
+		assert.NoError(t, shared.Set("777"))
+		assert.Equal(t, "777", shared.String())
+	})
+	t.Run("invalid shared", func(t *testing.T) {
+		var shared RepoShared
+		assert.NotNil(t, shared.Set("gogh"))
+		assert.NotNil(t, shared.Set("800"))
+	})
+	t.Run("invalid shared (triple term)", func(t *testing.T) {
+		var shared RepoShared
+		assert.NotNil(t, shared.Set("github.com/kyoh86/gogh"))
+	})
+}
+
 func TestRepoName(t *testing.T) {
+	t.Run("valid name", func(t *testing.T) {
+		var name RepoName
+		require.NoError(t, name.Set("kyoh86/gogh"))
+		assert.Equal(t, "kyoh86/gogh", name.String())
+		assert.Equal(t, "kyoh86", name.User())
+		assert.Equal(t, "gogh", name.Name())
+	})
+	t.Run("invalid name (single term)", func(t *testing.T) {
+		var name RepoName
+		assert.NotNil(t, name.Set("gogh"))
+	})
+	t.Run("invalid name (triple term)", func(t *testing.T) {
+		var name RepoName
+		assert.NotNil(t, name.Set("github.com/kyoh86/gogh"))
+	})
 }
 
 func TestRepoSpec(t *testing.T) {
