4f4f24ba736cbd5d1437b22d66c3b35e62ace751
Kyoichiro Yamada <me@kyoh86.dev>
2021-04-14 08:44:41 +0900

    templates

--- .github/actions/pkgbuild/entrypoint.sh
+++ .github/actions/pkgbuild/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -exuo pipefail
+set -euo pipefail
 
 pacman -Syu --noconfirm --needed base-devel pacman-contrib
 
@@ -23,7 +23,16 @@ else
   export VERSION="2.0.1"
 fi
 export COMMIT="${GITHUB_SHA}"
-envsubst "\$VERSION \$COMMIT" < pkgbuild.template.sh > PKGBUILD
+export NAME="${GITHUB_REPOSITORY#*/}"
+export OWNER="${GITHUB_REPOSITORY%/*}"
+export REPOSITORY="${GITHUB_REPOSITORY}"
+export SERVER_URL="${GITHUB_SERVER_URL}"
+if [ -n "${EMAIL}" ]; then
+  export EMAIL=" <${EMAIL}>"
+fi
+export DESCRIPTION
+export LICENSE
+envsubst "\$VERSION \$COMMIT \$NAME \$DESCRIPTION \$LICENSE \$OWNER \$REPOSITORY \$SERVER_URL \$EMAIL" < PKGBUILD.tmpl > PKGBUILD
 chmod 777 PKGBUILD
 sudo -H -u builder updpkgsums PKGBUILD
 sudo -H -u builder makepkg --printsrcinfo PKGBUILD | sudo -H -u builder tee .SRCINFO
