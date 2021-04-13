cc458318182ee2d7f9ea093a0c787d153c0c06e2
Kyoichiro Yamada <me@kyoh86.dev>
2021-04-13 22:21:54 +0900

    install pacman-contrib

--- .github/actions/pkgbuild/entrypoint.sh
+++ .github/actions/pkgbuild/entrypoint.sh
@@ -9,7 +9,7 @@ cat << EOM >> /etc/pacman.conf
 Include = /etc/pacman.d/mirrorlist
 EOM
 
-pacman -Syu --noconfirm --needed base-devel pkgconf
+pacman -Syu --noconfirm --needed base-devel pacman-contrib
 
 # Makepkg does not allow running as root
 # Create a new user `builder`
