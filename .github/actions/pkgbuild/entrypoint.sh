b7157b2b2741f3f800c63d53f9039bb40c30a9fc
Kyoichiro Yamada <me@kyoh86.dev>
2021-04-14 07:36:33 +0900

    fix

--- .github/actions/pkgbuild/entrypoint.sh
+++ .github/actions/pkgbuild/entrypoint.sh
@@ -23,7 +23,7 @@ else
   export VERSION="2.0.1"
 fi
 export COMMIT="${GITHUB_SHA}"
-envsubst "\$VERSION \$COMMIT" < ../pkgbuild.template.sh > PKGBUILD
+envsubst "\$VERSION \$COMMIT" < pkgbuild.template.sh > PKGBUILD
 chmod 777 PKGBUILD
 sudo -H -u builder updpkgsums PKGBUILD
 sudo -H -u builder makepkg --printsrcinfo PKGBUILD | sudo -H -u builder tee .SRCINFO
