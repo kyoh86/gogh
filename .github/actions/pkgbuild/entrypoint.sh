4f0d9d5151bf514deef5ff2bc82548a6c20a83f6
Kyoichiro Yamada <me@kyoh86.dev>
2021-04-13 22:56:28 +0900

    create user

--- .github/actions/pkgbuild/entrypoint.sh
+++ .github/actions/pkgbuild/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -exuo pipefail
 
-pacman -Syyu --noconfirm --needed
 pacman -Syu --noconfirm --needed base-devel pacman-contrib
+
+# Makepkg does not allow running as root
+# Create a new user `builder`
+# `builder` needs to have a home directory because some PKGBUILDs will try to
+# write to it (e.g. for cache)
+useradd builder -m
+
+# When installing dependencies, makepkg will use sudo
+# Give user `builder` passwordless sudo access
+echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Give all users (particularly builder) full access to these files
+chmod -R a+rw .
 
 export VERSION="${GITHUB_REF##*/v}"
 if [[ "${VERSION}" =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
@@ -13,5 +25,5 @@ fi
 export COMMIT="${GITHUB_SHA}"
 envsubst "\$VERSION \$COMMIT" < pkgbuild.template.sh > PKGBUILD
 chmod 777 PKGBUILD
-updpkgsums
+sudo -H -u builder updpkgsums
 cat PKGBUILD
