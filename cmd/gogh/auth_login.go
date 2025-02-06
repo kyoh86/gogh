9e733831fdc54a034c49e06a9a3ba69068a32d2b
kyoh86 <me@kyoh86.dev>
2025-02-06 20:29:08 +0900

    fix: makes it can be authorized for device

--- cmd/gogh/auth_login.go
+++ cmd/gogh/auth_login.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"errors"
-
-	"context"
 	"fmt"
-	"github.com/AlecAivazis/survey/v2"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
 	"github.com/kyoh86/gogh/v2"
 	"github.com/spf13/cobra"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
-)
-
-var (
-	oauthConfig = &oauth2.Config{
-		ClientID:    "Ov23li6aEWIxek6F8P5L",
-		RedirectURL: "http://localhost",
-		Endpoint:    github.Endpoint,
-		Scopes:      []string{"repo"},
-	}
 )
 
 var loginFlags struct {
@@ -27,49 +19,152 @@ var loginFlags struct {
 	Password string
 }
 
+const clientID = "Ov23li6aEWIxek6F8P5L" // ここに正しいClient IDを設定
+
+type DeviceCodeResponse struct {
+	DeviceCode              string
+	UserCode                string
+	VerificationURI         string
+	VerificationURIComplete string
+	ExpiresIn               int
+	Interval                int
+}
+
+type TokenResponse struct {
+	AccessToken string
+	Scope       string
+	TokenType   string
+}
+
+type ErrorResponse struct {
+	Error            string
+	ErrorDescription string
+	ErrorURI         string
+}
+
 var loginCommand = &cobra.Command{
 	Use:   "login",
 	Short: "Login for the host and owner",
 	Args:  cobra.ExactArgs(0),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		authCodeURL := oauthConfig.AuthCodeURL("state", oauth2.AccessTypeOffline)
-
-		fmt.Printf("Visit the URL for the auth dialog: %v\n", authCodeURL)
-
-		var authCode string
-		if err := survey.AskOne(&survey.Input{
-			Message: "Enter the authorization code:",
-		}, &authCode); err != nil {
-			return err
+		oauthConfig := &oauth2.Config{
+			ClientID: clientID,
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://github.com/login/device/code",
+				TokenURL: "https://github.com/login/oauth/access_token",
+			},
+			Scopes: []string{"repo"},
 		}
 
-		token, err := oauthConfig.Exchange(ctx, authCode)
+		// Request device code
+		deviceCodeResp, err := requestDeviceCode(oauthConfig.ClientID)
 		if err != nil {
-			return fmt.Errorf("failed to exchange token: %w", err)
+			return fmt.Errorf("failed to request device code: %w", err)
 		}
 
-		tokens.Set(loginFlags.Host, loginFlags.User, token.AccessToken)
+		fmt.Printf("Visit %s and enter the code: %s\n", deviceCodeResp.VerificationURI, deviceCodeResp.UserCode)
+
+		// Poll for token
+		tokenResp, err := pollForToken(oauthConfig, deviceCodeResp)
+		if err != nil {
+			return fmt.Errorf("failed to poll for token: %w", err)
+		}
+
+		tokens.Set(loginFlags.Host, loginFlags.User, tokenResp.AccessToken)
+
+		fmt.Println("Login successful!")
 		return nil
 	},
 }
 
-func stringValidator(v func(s string) error) survey.Validator {
-	return func(i interface{}) error {
-		s, ok := i.(string)
-		if !ok {
-			return errors.New("invalid type")
+func requestDeviceCode(clientID string) (*DeviceCodeResponse, error) {
+	resp, err := http.PostForm("https://github.com/login/device/code", url.Values{
+		"client_id": {clientID},
+		"scope":     {"repo"},
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Device code response body: %s\n", string(body)) // デバッグ出力
+
+	values, err := url.ParseQuery(string(body))
+	if err != nil {
+		return nil, err
+	}
+
+	deviceCodeResp := &DeviceCodeResponse{
+		DeviceCode:              values.Get("device_code"),
+		UserCode:                values.Get("user_code"),
+		VerificationURI:         values.Get("verification_uri"),
+		VerificationURIComplete: values.Get("verification_uri_complete"),
+		ExpiresIn:               atoi(values.Get("expires_in")),
+		Interval:                atoi(values.Get("interval")),
+	}
+
+	return deviceCodeResp, nil
+}
+
+func atoi(str string) int {
+	val, err := strconv.Atoi(str)
+	if err != nil {
+		return 0
+	}
+	return val
+}
+
+func pollForToken(oauthConfig *oauth2.Config, deviceCodeResp *DeviceCodeResponse) (*TokenResponse, error) {
+	for {
+		time.Sleep(time.Duration(deviceCodeResp.Interval*2) * time.Second)
+
+		resp, err := http.PostForm(oauthConfig.Endpoint.TokenURL, url.Values{
+			"client_id":   {oauthConfig.ClientID},
+			"device_code": {deviceCodeResp.DeviceCode},
+			"grant_type":  {"urn:ietf:params:oauth:grant-type:device_code"},
+		})
+		if err != nil {
+			return nil, err
 		}
-		return v(s)
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Printf("Token response body: %s\n", string(body)) // デバッグ出力
+
+		values, err := url.ParseQuery(string(body))
+		if err != nil {
+			return nil, err
+		}
+
+		if resp.StatusCode == http.StatusOK && values.Get("error") == "" {
+			tokenResp := &TokenResponse{
+				AccessToken: values.Get("access_token"),
+				Scope:       values.Get("scope"),
+				TokenType:   values.Get("token_type"),
+			}
+			return tokenResp, nil
+		} else if values.Get("error") == "authorization_pending" {
+			continue
+		} else {
+			return nil, fmt.Errorf("error: %s, description: %s, uri: %s",
+				values.Get("error"), values.Get("error_description"), values.Get("error_uri"))
+		}
 	}
 }
 
 func init() {
-	loginCommand.Flags().
-		StringVarP(&loginFlags.Host, "host", "", gogh.DefaultHost, "Host name to login")
+	loginCommand.Flags().StringVarP(&loginFlags.Host, "host", "", gogh.DefaultHost, "Host name to login")
 	loginCommand.Flags().StringVarP(&loginFlags.User, "user", "", "", "User name to login")
-	loginCommand.Flags().
-		StringVarP(&loginFlags.Password, "password", "", "", `Password or developer private token
+	loginCommand.Flags().StringVarP(&loginFlags.Password, "password", "", "", `Password or developer private token
 
 You should generate personal access tokens with "Repository permissions":
 
