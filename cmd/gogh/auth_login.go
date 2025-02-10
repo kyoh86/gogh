a1b58933e35b470c137e53c9d844e423993c18ed
kyoh86 <me@kyoh86.dev>
2025-02-10 18:44:22 +0900

    use golang.or2/oauth to auth device

--- cmd/gogh/auth_login.go
+++ cmd/gogh/auth_login.go
@@ -4,12 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 	"os/exec"
-	"strconv"
-	"time"
 
 	"github.com/charmbracelet/huh"
 	"github.com/cli/browser"
@@ -25,15 +20,6 @@ var loginFlags struct {
 
 const clientID = "Ov23li6aEWIxek6F8P5L"
 
-type DeviceCodeResponse struct {
-	DeviceCode              string
-	UserCode                string
-	VerificationURI         string
-	VerificationURIComplete string
-	ExpiresIn               int
-	Interval                int
-}
-
 type TokenResponse struct {
 	AccessToken string
 	Scope       string
@@ -45,8 +31,6 @@ type ErrorResponse struct {
 	ErrorDescription string
 	ErrorURI         string
 }
-
-var scopes = []string{"repo", "delete_repo"}
 
 var loginCommand = &cobra.Command{
 	Use:     "login",
@@ -65,19 +49,19 @@ var loginCommand = &cobra.Command{
 				return err
 			}
 		}
-		host := loginFlags.Host
 
 		oauthConfig := &oauth2.Config{
 			ClientID: clientID,
 			Endpoint: oauth2.Endpoint{
-				AuthURL:  fmt.Sprintf("https://%s/login/device/code", host),
-				TokenURL: fmt.Sprintf("https://%s/login/oauth/access_token", host),
+				AuthURL:       fmt.Sprintf("https://%s/login/oauth/authorize", loginFlags.Host),
+				TokenURL:      fmt.Sprintf("https://%s/login/oauth/access_token", loginFlags.Host),
+				DeviceAuthURL: fmt.Sprintf("https://%s/login/device/code", loginFlags.Host),
 			},
-			Scopes: scopes,
+			Scopes: []string{"repo", "delete_repo"},
 		}
 
 		// Request device code
-		deviceCodeResp, err := requestDeviceCode(oauthConfig.ClientID, oauthConfig.Endpoint.AuthURL)
+		deviceCodeResp, err := oauthConfig.DeviceAuth(cmd.Context())
 		if err != nil {
 			return fmt.Errorf("failed to request device code: %w", err)
 		}
@@ -89,13 +73,14 @@ var loginCommand = &cobra.Command{
 		}
 
 		// Poll for token
-		tokenResp, err := pollForToken(oauthConfig, deviceCodeResp)
+		deviceCodeResp.Interval = deviceCodeResp.Interval + 1 // Add a second for safety
+		tokenResp, err := oauthConfig.DeviceAccessToken(cmd.Context(), deviceCodeResp)
 		if err != nil {
 			return fmt.Errorf("failed to poll for token: %w", err)
 		}
 
 		// Get user info
-		adaptor, err := github.NewAdaptor(context.Background(), host, tokenResp.AccessToken)
+		adaptor, err := github.NewAdaptor(context.Background(), loginFlags.Host, tokenResp.AccessToken)
 		if err != nil {
 			return fmt.Errorf("failed to create GitHub adaptor: %w", err)
 		}
@@ -104,93 +89,11 @@ var loginCommand = &cobra.Command{
 			return fmt.Errorf("failed to get authenticated user info: %w", err)
 		}
 
-		tokens.Set(host, user.GetLogin(), tokenResp.AccessToken)
+		tokens.Set(loginFlags.Host, user.GetLogin(), tokenResp.AccessToken)
 
 		fmt.Println("Login successful!")
 		return nil
 	},
-}
-
-func requestDeviceCode(clientID, authURL string) (*DeviceCodeResponse, error) {
-	resp, err := http.PostForm(authURL, url.Values{
-		"client_id": {clientID},
-		"scope":     scopes,
-	})
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	values, err := url.ParseQuery(string(body))
-	if err != nil {
-		return nil, err
-	}
-
-	deviceCodeResp := &DeviceCodeResponse{
-		DeviceCode:              values.Get("device_code"),
-		UserCode:                values.Get("user_code"),
-		VerificationURI:         values.Get("verification_uri"),
-		VerificationURIComplete: values.Get("verification_uri_complete"),
-		ExpiresIn:               atoi(values.Get("expires_in")),
-		Interval:                atoi(values.Get("interval")),
-	}
-
-	return deviceCodeResp, nil
-}
-
-func atoi(str string) int {
-	val, err := strconv.Atoi(str)
-	if err != nil {
-		return 0
-	}
-	return val
-}
-
-func pollForToken(oauthConfig *oauth2.Config, deviceCodeResp *DeviceCodeResponse) (*TokenResponse, error) {
-POLL_LOOP:
-	for {
-		time.Sleep(time.Duration(deviceCodeResp.Interval*2) * time.Second) // Intervalを2倍にしてリクエスト頻度を下げる
-
-		resp, err := http.PostForm(oauthConfig.Endpoint.TokenURL, url.Values{
-			"client_id":   {oauthConfig.ClientID},
-			"device_code": {deviceCodeResp.DeviceCode},
-			"grant_type":  {"urn:ietf:params:oauth:grant-type:device_code"},
-		})
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		values, err := url.ParseQuery(string(body))
-		if err != nil {
-			return nil, err
-		}
-
-		switch {
-		case resp.StatusCode == http.StatusOK && values.Get("error") == "":
-			tokenResp := &TokenResponse{
-				AccessToken: values.Get("access_token"),
-				Scope:       values.Get("scope"),
-				TokenType:   values.Get("token_type"),
-			}
-			return tokenResp, nil
-		case values.Get("error") == "authorization_pending":
-			continue POLL_LOOP
-		default:
-			return nil, fmt.Errorf("error: %s, description: %s, uri: %s",
-				values.Get("error"), values.Get("error_description"), values.Get("error_uri"))
-		}
-	}
 }
 
 func init() {
