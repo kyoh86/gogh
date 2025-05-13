31ea2a134a3961c32dfa1891436ec7eb42ec5e4a
kyoh86 <me@kyoh86.dev>
2025-05-13 11:15:03 +0900

    wip

--- ui/cli/view/time.go
+++ ui/cli/view/time.go
@@ -18,7 +18,7 @@ func FuzzyAgoAbbr(now time.Time, at time.Time) string {
 	if ago < 24*time.Hour {
 		return fmt.Sprintf("%d%s", int(ago.Hours()), "h")
 	}
-	if ago < 30*24*time.Hour {
+	if ago < 7*24*time.Hour && now.Month() == at.Month() {
 		return fmt.Sprintf("%d%s", int(ago.Hours())/24, "d")
 	}
 	return at.Format("2006-01-02")
