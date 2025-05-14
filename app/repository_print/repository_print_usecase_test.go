d2d5e567c2cc74ec6b5bc9d3d4556fdf9e71470c
kyoh86 <me@kyoh86.dev>
2025-05-14 09:50:15 +0900

    wip

--- app/repository_print/repository_print_usecase_test.go
+++ app/repository_print/repository_print_usecase_test.go
@@ -2,14 +2,24 @@ package repository_print_test
 
 import (
 	"bytes"
-	"io"
+	"iter"
 	"testing"
 	"time"
 
+	testtarget "github.com/kyoh86/gogh/v3/app/repository_print"
 	"github.com/kyoh86/gogh/v3/core/hosting"
 	"github.com/kyoh86/gogh/v3/core/repository"
-	testtarget "github.com/kyoh86/gogh/v3/ui/cli/view"
 )
+
+func sliceToIter2[T any](slices []T) iter.Seq2[T, error] {
+	return func(yield func(T, error) bool) {
+		for _, v := range slices {
+			if !yield(v, nil) {
+				return
+			}
+		}
+	}
+}
 
 func TestRepositoryPrinter(t *testing.T) {
 	uat, err := time.Parse(time.RFC3339, "2021-05-01T01:00:00Z")
@@ -23,33 +33,30 @@ func TestRepositoryPrinter(t *testing.T) {
 		URL:       "https://github.com/kyoh86/gogh",
 	}
 	for _, testcase := range []struct {
-		title   string
-		printer func(io.Writer) testtarget.RepositoryPrinter
-		want    string
+		title  string
+		format string
+		want   string
 	}{
 		{
-			title:   "ref",
-			printer: testtarget.NewRepositoryPrinterRef,
-			want:    "github.com/kyoh86/gogh\n",
+			title:  "ref",
+			format: "ref",
+			want:   "github.com/kyoh86/gogh\n",
 		},
 		{
-			title:   "url",
-			printer: testtarget.NewRepositoryPrinterURL,
-			want:    "https://github.com/kyoh86/gogh\n",
+			title:  "url",
+			format: "url",
+			want:   "https://github.com/kyoh86/gogh\n",
 		},
 		{
-			title:   "json",
-			printer: testtarget.NewRepositoryPrinterJSON,
-			want:    `{"ref":{"host":"github.com","owner":"kyoh86","name":"gogh"},"url":"https://github.com/kyoh86/gogh","updatedAt":"2021-05-01T01:00:00Z"}` + "\n",
+			title:  "json",
+			format: "json",
+			want:   `{"ref":{"host":"github.com","owner":"kyoh86","name":"gogh"},"url":"https://github.com/kyoh86/gogh","updatedAt":"2021-05-01T01:00:00Z"}` + "\n",
 		},
 	} {
 		t.Run(testcase.title, func(t *testing.T) {
 			var buf bytes.Buffer
-			printer := testcase.printer(&buf)
-			if err := printer.Print(repo); err != nil {
-				t.Fatal(err)
-			}
-			if err := printer.Close(); err != nil {
+			printer := testtarget.NewUseCase(&buf, testcase.format)
+			if err := printer.Execute(sliceToIter2([]*hosting.Repository{&repo})); err != nil {
 				t.Fatal(err)
 			}
 			got := buf.String()
