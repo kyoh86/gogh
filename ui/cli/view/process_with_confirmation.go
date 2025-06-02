03942976874d1b8e20c6f42cb0df259265cb6cbc
kyoh86 <me@kyoh86.dev>
2025-06-02 12:40:12 +0900

    accelt "all" for each process confirmataion

--- ui/cli/view/process_with_confirmation.go
+++ ui/cli/view/process_with_confirmation.go
@@ -8,11 +8,19 @@ import (
 	"github.com/charmbracelet/huh"
 )
 
+// ProcessWithConfirmation processes each entry in the sequence with a confirmation prompt.
 func ProcessWithConfirmation[T any](ctx context.Context, seq iter.Seq2[T, error], title func(T) string, process func(entry T) error) error {
 	logger := log.FromContext(ctx)
+	var all bool
 	for entry, err := range seq {
 		if err != nil {
 			return err
+		}
+		if all {
+			if err := process(entry); err != nil {
+				return err
+			}
+			continue
 		}
 		var selected string
 		if err := huh.NewForm(huh.NewGroup(
@@ -27,12 +35,18 @@ func ProcessWithConfirmation[T any](ctx context.Context, seq iter.Seq2[T, error]
 				}, huh.Option[string]{
 					Key:   "q",
 					Value: "Quit",
+				}, huh.Option[string]{
+					Key:   "a",
+					Value: "All",
 				}).
 				Value(&selected),
 		)).Run(); err != nil {
 			return err
 		}
 		switch selected {
+		case "All", "a":
+			all = true
+			fallthrough
 		case "Yes", "y":
 			if err := process(entry); err != nil {
 				return err
