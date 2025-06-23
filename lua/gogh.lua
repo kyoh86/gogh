9d8a3829a439d8e1cdda40b7763d0a3c0d6ac328
kyoh86 <me@kyoh86.dev>
2025-06-24 00:35:50 +0900

    fix lua definitions

--- lua/gogh.lua
+++ lua/gogh.lua
@@ -5,8 +5,18 @@
 ---@field owner string
 ---@field name string
 
+---@class gogh.Hook
+---@field id string Hook UUID
+---@field name string Hook name
+---@field repoPattern string Pattern that matched
+---@field triggerEvent string Event that triggered the hook
+---@field operationType string Type of operation that triggered the hook: "script" always.
+---@field operationId string Operation UUID (script UUID)
+
 ---@class gogh
 ---@field repo gogh.Repo
+---@field hook gogh.Hook
+---@field parent? gogh.Repo|nil
 
 ---@type gogh.Repo
 local repo = {
@@ -16,5 +26,16 @@ local repo = {
   owner = "",
   name = "",
 }
+
+---@type gogh.Hook
+local hook = {
+  id = "",
+  name = "",
+  repoPattern = "",
+  triggerEvent = "",
+  operationType = "script",
+  operationId = "",
+}
+
 ---@type gogh
-_G.gogh = { repo = repo }
+_G.gogh = { repo = repo, hook = hook }
